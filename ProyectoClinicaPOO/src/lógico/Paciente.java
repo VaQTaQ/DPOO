e3d5c67e35311a9534eb8558ee72d66647ca9534
Usuario <Usuario@HugoFerD3LL>
2024-11-23 12:31:23 -0400

    creando Doctor y Persona y llenando atributos junto a Paciente

--- ProyectoClinicaPOO/src/lógico/Paciente.java
+++ ProyectoClinicaPOO/src/lógico/Paciente.java
@@ -1,5 +1,28 @@
 package lógico;
 
-public class Paciente {
+import java.util.ArrayList;
 
+public class Paciente extends Persona{
+	private ArrayList<Vacuna> misVacunas;
+	private ArrayList<Enfermedad> enfermedades;
+	private Reporte historiaClinica;
+	
+	public ArrayList<Vacuna> getMisVacunas() {
+		return misVacunas;
+	}
+	public void setMisVacunas(ArrayList<Vacuna> misVacunas) {
+		this.misVacunas = misVacunas;
+	}
+	public ArrayList<Enfermedad> getEnfermedades() {
+		return enfermedades;
+	}
+	public void setEnfermedades(ArrayList<Enfermedad> enfermedades) {
+		this.enfermedades = enfermedades;
+	}
+	public Reporte getHistoriaClinica() {
+		return historiaClinica;
+	}
+	public void setHistoriaClinica(Reporte historiaClinica) {
+		this.historiaClinica = historiaClinica;
+	}
 }
---
+++ ProyectoClinicaPOO/src/lógico/Persona.java
@@ -0,0 +1,47 @@
+package lógico;
+
+public abstract class Persona {
+	protected String cedula;
+	protected String nombre;
+	protected String apellido;
+	protected int edad;
+	protected String sexo;
+	protected String direccion;
+	public String getCedula() {
+		return cedula;
+	}
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+}
