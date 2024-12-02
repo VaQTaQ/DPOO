0a6ee8eb9650a98c3daf4fdf6ca8c79d01a5a808
chala <chala@Charls>
2024-12-02 17:46:32 -0400

    Constructor creado y comentacion de ideas para ese apartado

--- ProyectoClinicaPOO/src/lógico/Historial.java
+++ ProyectoClinicaPOO/src/lógico/Historial.java
@@ -9,8 +9,49 @@ public class Historial {
 	private Date Fecha;
 	private String Detalle;
 	
+	public Historial(){//constructor con ambas ideas implementadas(si se elije la de hugo se deberia eliminar detalles y accionn)
+		super();//sino los arrayList
+		
+		this.Accion = new String();
+		this.Fecha = new Date();
+		this.Detalle = new String();
+		this.Acciones = new ArrayList<Object>();
+		this.reportes = new ArrayList<String>();
+	}
+
+	
 	//idea de Hugo
 	private ArrayList<Object> Acciones; //incluira todo lo que se haga en la clinica(ejemplo: se pone la vacuna a un paciente ese objeto Vacuna iria dentro de ese arrayLis)
 	private ArrayList<String> reportes;//Aqui iran los reportes de todo lo que se haga que se generaran de forma automatica con funciones
+	public String getAccion() {
+		return Accion;
+	}
+	public void setAccion(String accion) {
+		Accion = accion;
+	}
+	public Date getFecha() {
+		return Fecha;
+	}
+	public void setFecha(Date fecha) {
+		Fecha = fecha;
+	}
+	public String getDetalle() {
+		return Detalle;
+	}
+	public void setDetalle(String detalle) {
+		Detalle = detalle;
+	}
+	public ArrayList<Object> getAcciones() {
+		return Acciones;
+	}
+	public void setAcciones(ArrayList<Object> acciones) {
+		Acciones = acciones;
+	}
+	public ArrayList<String> getReportes() {
+		return reportes;
+	}
+	public void setReportes(ArrayList<String> reportes) {
+		this.reportes = reportes;
+	}
 	
 }
