0baa5eb8ae890a416e42452cdcdea10c88eff5d7
Usuario <Usuario@HugoFerD3LL>
2024-12-02 13:52:15 -0400

    creando clase usuario con tipos en booleano y string

---
+++ ProyectoClinicaPOO/src/lógico/User.java
@@ -0,0 +1,59 @@
+package lógico;
+
+public class User {
+	private String nombre;
+	private String pasword;
+	private String type;
+	private boolean isAdmint;
+	private boolean isMedic;
+	private boolean isPaciente;
+	
+	public User(String nombre, String pasword, String type, boolean isAdmint, boolean isMedic, boolean isPaciente) {
+		super();
+		this.nombre = nombre;
+		this.pasword = pasword;
+		this.type = type;
+		this.isAdmint = isAdmint;
+		this.isMedic = isMedic;
+		this.isPaciente = isPaciente;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getPasword() {
+		return pasword;
+	}
+	public void setPasword(String pasword) {
+		this.pasword = pasword;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public boolean isAdmint() {
+		return isAdmint;
+	}
+	public void setAdmint(boolean isAdmint) {
+		this.isAdmint = isAdmint;
+	}
+	public boolean isMedic() {
+		return isMedic;
+	}
+	public void setMedic(boolean isMedic) {
+		this.isMedic = isMedic;
+	}
+	public boolean isPaciente() {
+		return isPaciente;
+	}
+	public void setPaciente(boolean isPaciente) {
+		this.isPaciente = isPaciente;
+	}
+	
+
+}
